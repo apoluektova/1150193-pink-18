94b7b93b3655341397cd90c2b6518b85ae2b078a
Анна Полуэктова <a.poluektova88@gmail.com>
2020-01-26 17:25:35 +0400

    ПП для модалок и интерактивная карта

---
+++ source/js/map.js
@@ -0,0 +1,19 @@
+ymaps.ready(init);
+    function init(){
+        var myMap = new ymaps.Map("map", {
+          center: [59.938867, 30.323133],
+          zoom: 17,
+          controls: []
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+        var myPlacemark = new ymaps.Placemark([59.938867, 30.323133] , {},
+          { iconLayout: 'default#image',
+            iconImageHref: 'img/svg/icon-map-marker.svg',
+            iconImageSize: [36, 35],
+            iconImageOffset: [-20, 15]
+          });
+
+          myMap.geoObjects.add(myPlacemark);
+      }
